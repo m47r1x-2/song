962928b7c538a3d3c5dc406918b81d24e39127b2
M47R1X <85056526+m47r1x-2@users.noreply.github.com>
2021-06-26 01:28:54 +0530

    Add files via upload

--- run.sh
+++ run.sh
@@ -12,11 +12,24 @@ pkg install python
 apt install mpg123
 
 figlet you
+
+sleep 1
+
 figlet have
+
+sleep 1
+
 figlet been
+
+sleep 1
+
 figlet hacked
 
+
 sleep 2
+
 cd $HOME
+
 cd song
+
 python song.py
