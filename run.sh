8e2f45dfb4384244df08f25aafb9ed0d85cda3e7
M47R1X <85056526+m47r1x-2@users.noreply.github.com>
2021-06-25 21:47:11 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bashclear
+
+###############################################################
+
+clear
+
+apt update && apt upgrade
+
+pkg install python
+
+
+apt install mpg123
+
+figlet you
+figlet have
+figlet been
+figlet hacked
+
+sleep 2
+cd $HOME
+cd song
+python song.py
---
+++ song.py
@@ -0,0 +1,9 @@
+# apt install mpg123
+
+import os
+
+file = "d.mp3"
+
+os.system("mpg123 " + file)
+
+print("You Have Been Hacked")
